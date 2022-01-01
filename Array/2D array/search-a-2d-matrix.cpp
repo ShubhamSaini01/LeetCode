135236f129ad7645fb40f39ec8a9f625192a90d4
ShubSaini001 <shubham.saini@linoy.in>
2022-01-01 11:51:14 +0530

    Array problems

---
+++ Array/2D array/search-a-2d-matrix.cpp
@@ -0,0 +1,16 @@
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int m = matrix.size();
+    int n =matrix[0].size();
+    for(int i=0;i<m;i++){
+        if(matrix[i][n-1]== target)
+            return true;
+        if(matrix[i][n-1]> target){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]== target )
+                    return true;
+            }
+        }
+
+    }
+    return false;
+}
